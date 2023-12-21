03c90944bdec8501eaf06664e3ae32da35d1faa7
Yuvalkleinn <154416324+Yuvalkleinn@users.noreply.github.com>
2023-12-21 11:57:16 +0200

    Add files via upload

--- AddTwo.java
+++ AddTwo.java
@@ -1,8 +1,11 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
-public class AddTwo {
-	public static void main(String[] args) {
-		// Put your code here	
-	}
+class AddTwo {
+    public static void main(String args[]) {
+
+        // Parse command-line arguments
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        // Print the result
+        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+    }
 }
