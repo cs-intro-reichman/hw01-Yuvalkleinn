24c6409a623e44b48d9c072cab2a15aeb92bb0d3
yuvalkleinn <yuval.klein01@runi.post.ac.il>
2023-12-21 21:55:45 +0200

    Final Solution

--- AddTwo.java
+++ AddTwo.java
@@ -2,10 +2,10 @@ class AddTwo {
     public static void main(String args[]) {
 
         // Parse command-line arguments
-        int num1 = Integer.parseInt(args[0]);
-        int num2 = Integer.parseInt(args[1]);
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
 
         // Print the result
-        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+        System.out.println(x + " + " + y + " = " + (x + y));
     }
 }
