e7cdb621a552574a8a1bc7a3ca6b977bd96f9c9b
yuvalkleinn <yuval.klein01@runi.post.ac.il>
2023-12-22 11:59:23 +0200

    Update GenThree

--- GenThreeTest.java
+++ GenThreeTest.java
@@ -12,12 +12,10 @@ class GenThreeTest {
         int random1 = random.nextInt(Math.max(num1, num2) - Math.min(num1, num2)) + Math.min(num1, num2);
         int random2 = random.nextInt(Math.max(num1, num2) - Math.min(num1, num2)) + Math.min(num1, num2);
         int random3 = random.nextInt(Math.max(num1, num2) - Math.min(num1, num2)) + Math.min(num1, num2);
-        int min_num = Math.min(Math.min(random1, random2), random3)
+        int min_num = Math.min(Math.min(random1, random2), random3);
 
         // Print the generated random numbers and the minimal number
-        System.out.println(random1);
-        System.out.println(random2);
-        System.out.println(random3);
+        System.out.println(random1 + "\n" + random2 + "\n" + random3);
         System.out.println("The minimal generated number was " + min_num);
     }
 }
